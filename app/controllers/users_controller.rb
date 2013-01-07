422b22f5b7b135f1008738307df38aca42282ea9
Harry Moreno <morenoh149@gmail.com>
2013-01-06 20:24:30 -0800

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,25 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
   end
+
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
+
   def new
     if signed_in?
       redirect_to root_path
     end
     @user = User.new
   end
+
   def create
     if signed_in?
       redirect_to root_path
@@ -29,13 +33,16 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
     redirect_to users_url
   end
+
   def edit
   end
+
   def update
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
@@ -45,12 +52,28 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
     
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+    
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
